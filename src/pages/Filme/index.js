68c779facfb7a411650d26fdbd3eb941d7c56801
Pedro Henrique <168768075+DevPedroPinheiro@users.noreply.github.com>
2025-09-25 11:39:06 -0300

    prime

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../../services/api";
+import "./filme-info.css";
+import {toast} from "react-toastify";
+
+function Filme() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [filme, setFilme] = useState([]);
+  const [loading, setLoading] = useState([true]);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`movie/${id}`, {
+          params: {
+            api_key: "585f8ab12abf811e79a4eaf3c0f7dfe0",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          navigate("/", { replace: true });
+          return;
+        });
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO");
+    };
+  }, [id, navigate]);
+
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some(
+      (filmesSalvos) => filmesSalvos.id === filme.id
+    );
+
+    if (hasFilme) {
+      toast.warn("ESSE FILME JÁ ESTA NA LISTA");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    toast.success("FILME SALVO COM SUCESSO");
+  }
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando Detalhe...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+      <div className="area-buttons">
+        <button onClick={salvarFilme}>Salvar</button>
+        <button>
+          <a
+            href={`https://youtube.com/results?search_query=${filme.title} trailer`}
+            target="blank"
+            rel="external"
+          >
+            Trailer
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filme;
